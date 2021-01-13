311ac94a367bf30e3e8459c4f2a61873e7883961
Filipan <filipandani@gamil.com>
2021-01-13 21:03:07 +0200

    Add project files.

---
+++ Models/Drink.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Filipan_Dani_Proiect.Models
+{
+    public class Drink
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Brand { get; set; }
+        public int Liters { get; set; }
+        public ICollection<Order> Orders { get; set; }
+
+    }
+}
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Filipan_Dani_Proiect.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
